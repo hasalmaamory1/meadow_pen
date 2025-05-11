7aaeae0fb4e1847fdbe15b67177d24bf47584458
hasalmaamory1 <hasalmaamory@gmail.com>
2025-05-11 01:40:52 -0600

    Initial commit

--- meadow_pen_clean/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
-      <h1>✅ Hello from MeadowPen!</h1>
-      <p>If you see this, Vite is working.</p>
-    </div>
-  );
-}
